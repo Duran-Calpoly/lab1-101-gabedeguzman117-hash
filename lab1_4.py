cb3c029a948ce262445806f5d109d9d4b401a6de
gabedeguzman117-hash <gabedeguzman117@gmail.com>
2026-01-17 20:53:32 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,10 @@
-pass
+def calculate_average(num1, num2, num3):
+    return (num1+num2+num3)/3
+
+def add_tax(bill_total):
+    tax_rate = 0.10
+    tax_amount = tax_rate*bill_total
+    return bill_total+tax_amount 
+
+def greet_user(name):
+    return f"Hello {name}"
